93cd8169bfb1f9b6cb3bcdbaa359587f3ac0364b
Yang Su <suyang1+services@gmail.com>
2012-10-30 12:11:15 -0400

    Added tangle

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -225,4 +225,16 @@ $(document).ready(function(){
   }
   parse();
 
+  var tangle = new Tangle (document.getElementById("calorieCalculator"), {
+    initialize: function () {
+        this.cookies = 3;
+        this.caloriesPerCookie = 50;
+        this.dailyCalories = 2100;
+    },
+    update: function () {
+        this.calories = this.cookies * this.caloriesPerCookie;
+        this.dailyPercent = 100 * this.calories / this.dailyCalories;
+    }
+  });
+
 });
